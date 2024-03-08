391aef5aef1711b1a302ba77aad7a41ffe89130e
Nikita <barskii.no@outlook.com>
2024-03-08 12:26:07 +0300

    Add API endpoint to get place info

--- places/views.py
+++ places/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 from places.models import TourCompany
 
@@ -26,3 +26,28 @@ def index(request):
         }
         places_json['features'].append(new_feature)
     return render(request, 'index.html', context={"places": places_json})
+
+
+def places(request, place_id):
+    place = get_object_or_404(TourCompany, id=place_id)
+    data = {
+        "title": place.title,
+        "imgs": [],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+    for image in place.images.all():
+        data["imgs"].append(image.image.url)
+
+    return JsonResponse(
+        data,
+        json_dumps_params={
+            'indent': 2,
+            'ensure_ascii': False
+        }
+    )
+
