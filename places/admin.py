1bdd5c9c4d6cffceba13b4b80f85dbab96b6f7e7
Nikita <barskii.no@outlook.com>
2024-03-14 15:51:28 +0300

    Fix admin panel

--- places/admin.py
+++ places/admin.py
@@ -5,24 +5,33 @@ from django.utils.html import format_html
 from places.models import TourCompany, TourImage
 
 
+MAX_WIDTH = 200
+MAX_HEIGHT = 200
+
+
 class TourCompanyInline(SortableInlineAdminMixin, admin.TabularInline):
     model = TourImage
     extra = 1
-    readonly_fields = ['tour_image', ]
-    fields = ('image', 'tour_image', 'position',)
+    readonly_fields = ['get_image_tag', ]
+    fields = ('image', 'get_image_tag', 'position',)
 
-    def tour_image(self, obj):
+    def get_image_tag(self, obj):
         return format_html(
-            "<img src='{url}' height=200px>",
-            url=obj.image.url)
+            "<img src='{url}' style='max-width: {MAX_WIDTH}px; max-height: {MAX_HEIGHT}px;'>",
+            url=obj.image.url,
+            MAX_WIDTH=MAX_WIDTH,
+            MAX_HEIGHT=MAX_HEIGHT
+        )
 
 
 @admin.register(TourCompany)
 class TourCompanyAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ('title',)
     inlines = [TourCompanyInline, ]
+    search_fields = ('title',)
 
 
 @admin.register(TourImage)
 class TourImageAdmin(admin.ModelAdmin):
     ordering = ('position',)
+    autocomplete_fields = ('tour_company',)
