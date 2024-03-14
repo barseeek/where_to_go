74ff9f38da9c7d272115119f7bc7d2321c7bc649
Nikita <barskii.no@outlook.com>
2024-03-14 20:27:41 +0300

    Fix load_place, optimize functions add check for exceptions and defaults

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,6 +1,7 @@
 import logging
 
 import requests
+from django.core.exceptions import MultipleObjectsReturned
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
 
@@ -11,31 +12,32 @@ def fetch_url(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response.json()
+        return response
     except requests.ConnectionError as e:
         logging.error('Connection error: {}'.format(e))
     except requests.RequestException as e:
         logging.error('RequestException error: {}'.format(e))
 
 
-def create_images(place, images):
-    for num, link in enumerate(images):
-        logging.info(f'Try loading image from {link}')
-        try:
-            response = requests.get(link)
-            response.raise_for_status()
-            filename = link.strip().split('/')[-1]
-            image, created = TourImage.objects.get_or_create(tour_company=place,
-                                                             position=num,
-                                                             image=ContentFile(name=filename,
-                                                                               content=response.content)
-                                                             )
-            if created:
-                logging.info('Successfully created image {} for place {}'.format(filename, place.title))
-        except requests.ConnectionError as e:
-            logging.error('Failed to load {}, connection error: {}'.format(link, e))
-        except requests.RequestException as e:
-            logging.error('Failed to load {}, requestException error: {}'.format(link, e))
+def create_image(place, link, num):
+    logging.info(f'Try loading image from {link}')
+    response = fetch_url(link)
+    if not response:
+        logging.error(f'Response from {link} is None')
+    if not response.content:
+        logging.error(f'Can\'t load image from {link}, no content ')
+        return
+    filename = link.strip().split('/')[-1]
+    try:
+        image, created = TourImage.objects.get_or_create(tour_company=place,
+                                                         position=num,
+                                                         image=ContentFile(name=filename,
+                                                                           content=response.content)
+                                                         )
+        if created:
+            logging.info('Successfully created image {} for place {}'.format(filename, place.title))
+    except MultipleObjectsReturned:
+        logging.error(f'Photo {filename} is already exists')
 
 
 class Command(BaseCommand):
@@ -52,21 +54,26 @@ class Command(BaseCommand):
 
     def handle(self, get_or_create=None, *args, **options):
         url = options['url']
-        payload = fetch_url(url)
+        response = fetch_url(url)
+        if not response:
+            logging.error(f'Response from {url} is None')
+        payload = response.json()
         if not payload:
             logging.error("Can't get json from {}".format(url))
             return
 
         place, created = TourCompany.objects.get_or_create(
             title=payload['title'],
-            short_description=payload['description_short'],
-            long_description=payload['description_long'],
-            lng=payload['coordinates']['lng'],
-            lat=payload['coordinates']['lat']
-
+            defaults={
+                'short_description': payload['description_short'],
+                'long_description': payload['description_long'],
+                'lng': payload['coordinates']['lng'],
+                'lat': payload['coordinates']['lat']
+            }
         )
         if created:
             logging.info('Successfully created place:{}'.format(place))
-            create_images(place, payload['imgs'])
+            for num, url in enumerate(payload['imgs']):
+                create_image(place, url, num)
         else:
             logging.info('Place already exists:{}'.format(place))
--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from tinymce.models import HTMLField
 
 
 class TourCompany(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
+    title = models.CharField(max_length=255, verbose_name='Название', unique=True)
     short_description = models.TextField(null=True, blank=True, verbose_name='Краткое описание')
     long_description = HTMLField(null=True, blank=True, verbose_name='Подробное описание')
     lng = models.DecimalField(max_digits=20, decimal_places=15, verbose_name='Долгота')
